4fce2530edd9446b7d7bc532c5aab4b36b06faf9
Saisuchendra Geddada <61021066+geddadasaisuchendra@users.noreply.github.com>
2025-12-14 17:18:17 +0530

    Update Footer.jsx

--- room-booking/src/components/layout/Footer.jsx
+++ room-booking/src/components/layout/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="site-footer py-3 mt-4">
       <div className="container text-center text-white small">
-        © {new Date().getFullYear()} Bima Grand residency — All Rights Reserved.
+        © {new Date().getFullYear()} Bheema Grand residency — All Rights Reserved.
       </div>
     </footer>
   );
