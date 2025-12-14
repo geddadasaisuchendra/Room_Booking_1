8d965bd52ef44cc2441b96efaf4b2bcd6bf5967a
Saisuchendra Geddada <61021066+geddadasaisuchendra@users.noreply.github.com>
2025-12-14 17:17:26 +0530

    Update HomePage.jsx

--- room-booking/src/pages/HomePage.jsx
+++ room-booking/src/pages/HomePage.jsx
@@ -21,22 +21,20 @@ export default function HomePage() {
 
         {/* Rules ABOVE calendar */}
         <div className="rules-wrapper">
-          <h4 className="rules-title">Kind attention to all devotees visiting Mantralayam</h4>
+          <h4 className="rules-title">Kind attention to Customers visiting Bheema Grand Residency</h4>
           <div className="rules-box">
           <ul>
-            <li>Booking is for one day only</li>
+            <li>Booking can be done for one to two days.</li>
             <li>No cancellation, refund, or date/time change allowed.</li>
-            <li>You may book up to two rooms per login ID in a month.</li>
-            <li>Rooms will not be allotted to a single person. Such bookings will be cancelled and money will not be refunded.</li>
             <li>Each room can accommodate two to four adults.</li>
             <li>No extra bed or bedding will be provided.</li>
-            <li>You may book rooms between 3 and 15 days in advance.</li>
-            <li>if bookings are in pending state please wait for 5mins</li>
+            <li>You may book rooms instently and also in advance.</li>
             <li>A successful payment does not guarantee a booking. Wait for the confirmation message shown on the screen. Return to this page after completing your UPI payment.</li>
             <li>A refundable deposit equal to one day's room tariff must be paid in cash at the time of check-in.</li>
-            <li>Bima Grand Residency reserves the right to cancel any booking at any time and refund the amount in full.</li>
-            <li>Rooms can be booked in advance, only on this website - bimagrandresidency.com or in person at CRO in Mantralayam</li>
-            <li>No Email/WA/Phone booking/enquiry. Please do not call for rooms.</li>
+            <li>Bheema Grand Residency reserves the right to cancel any booking at any time and refund the amount in full.</li>
+            <li>Rooms can be booked in advance, only on this website - bheemagrandresidency.com or in person at  Bheema Grand Residency, Mantralayam</li>
+            <li>you can book via Email/WA/Phone booking/enquiry.</li>
+            <li>contact us at +91 9603527758</li>
 
           </ul>
            </div>
