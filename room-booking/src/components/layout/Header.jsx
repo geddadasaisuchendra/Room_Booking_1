dd6f38708b184f745e63b9e9a275924b7a1e2ad7
Saisuchendra Geddada <61021066+geddadasaisuchendra@users.noreply.github.com>
2025-12-26 13:45:08 +0530

    Update Header.jsx

--- room-booking/src/components/layout/Header.jsx
+++ room-booking/src/components/layout/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "../../assets/logo.png"; // placeholder logo
+import logo from "../../assets/s2.png"; // placeholder logo
 
 export default function Header() {
   return (
