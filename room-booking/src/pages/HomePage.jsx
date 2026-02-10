77a216df53061aa330f44e71f05c4c3b869412fe
Saisuchendra Geddada <61021066+geddadasaisuchendra@users.noreply.github.com>
2026-02-10 18:43:01 +0530

    Update HomePage.jsx

--- room-booking/src/pages/HomePage.jsx
+++ room-booking/src/pages/HomePage.jsx
@@ -24,14 +24,18 @@ export default function HomePage() {
           <h4 className="rules-title">Kind attention to customers visiting Bheema Grand Residency</h4>
           <div className="rules-box">
           <ul>
+            <li>Located just 1.5 km from the temple for quick and easy access</li>
+            <li>ree car parking available for all guests</li>
+            <li>Complimentary pick-up and drop service between the room and temple (one round trip per stay)</li>
+            <li>24-hour hot water for your comfort</li>
+            <li>Power backup to ensure an uninterrupted stay</li>
+            
             <li>Booking can be done for one to two days.</li>
             <li>No cancellation, refund, or date/time change allowed.</li>
             <li>Each room can accommodate two to four adults.</li>
             <li>No extra bed or bedding will be provided.</li>
             <li>You may book rooms instently and also in advance.</li>
             <li>A successful payment does not guarantee a booking. Wait for the confirmation message shown on the screen. Return to this page after completing your UPI payment.</li>
-            <li>A refundable deposit equal to one day's room tariff must be paid in cash at the time of check-in.</li>
-            <li>Bheema Grand Residency reserves the right to cancel any booking at any time and refund the amount in full.</li>
             <li>Rooms can be booked in advance, only on this website - bheemagrandresidency.com or in person at  Bheema Grand Residency, Mantralayam</li>
             <li>you can book via Email/WA/Phone booking/enquiry.</li>
             <li>contact us at +91 9603527758</li>
